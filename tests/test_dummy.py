323ebd214514b1a1bc81d6606559aedea9e1a56c
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2023-04-18 17:04:23 +0200

    refactor for analysis and testability in ci

---
+++ tests/test_dummy.py
@@ -0,0 +1,12 @@
+import mock
+import unittest
+
+
+class TestDummy(unittest.TestCase):
+
+    @unittest.skip("dummy test")
+    def test_main(self):
+        """Dummy test."""
+        main = mock.Mock()
+        main.return_value = None
+        assert None is main()
