6ebd28169f45d691c493826f5493c5624a8bb7b0
Nacho <nacho@sixsq.com>
2023-04-20 08:47:19 +0200

    Peripheral DB manager template

---
+++ tests/peripherals/test_peripheral_manager_db.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+
+
+class TestPeripheralsDBManager(TestCase):
+    def test_content(self):
+        self.fail()
+
+    def test_keys(self):
+        self.fail()
+
+    def test_synchronize(self):
+        self.fail()
+
+    def test_update_local_storage(self):
+        self.fail()
+
+    def test_decode_new_peripherals(self):
+        self.fail()
+
+    def test_add_peripheral(self):
+        self.fail()
+
+    def test_add_local_peripheral(self):
+        self.fail()
+
+    def test_add_remote_peripheral(self):
+        self.fail()
+
+    def test_remove_peripheral(self):
+        self.fail()
+
+    def test_remove_local_peripheral(self):
+        self.fail()
+
+    def test_remove_remote_peripheral(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_peripheral_expired(self):
+        self.fail()
+
+    def test_remove(self):
+        self.fail()
+
+    def test_edit(self):
+        self.fail()
